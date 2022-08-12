4b83b9a6fe047ff10191650dba84312d572d6dba
aberhamm <matthew.aberham@gmail.com>
2022-08-12 17:15:18 -0400

    font-display swap

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -25,59 +25,59 @@ import SFMonoSemiboldItalicWoff from '@fonts/SFMono/SFMono-SemiboldItalic.woff';
 import SFMonoSemiboldItalicWoff2 from '@fonts/SFMono/SFMono-SemiboldItalic.woff2';
 
 const calibreNormalWeights = {
-  400: [CalibreRegularWoff, CalibreRegularWoff2],
-  500: [CalibreMediumWoff, CalibreMediumWoff2],
-  600: [CalibreSemiboldWoff, CalibreSemiboldWoff2],
+    400: [CalibreRegularWoff, CalibreRegularWoff2],
+    500: [CalibreMediumWoff, CalibreMediumWoff2],
+    600: [CalibreSemiboldWoff, CalibreSemiboldWoff2],
 };
 
 const calibreItalicWeights = {
-  400: [CalibreRegularItalicWoff, CalibreRegularItalicWoff2],
-  500: [CalibreMediumItalicWoff, CalibreMediumItalicWoff2],
-  600: [CalibreSemiboldItalicWoff, CalibreSemiboldItalicWoff2],
+    400: [CalibreRegularItalicWoff, CalibreRegularItalicWoff2],
+    500: [CalibreMediumItalicWoff, CalibreMediumItalicWoff2],
+    600: [CalibreSemiboldItalicWoff, CalibreSemiboldItalicWoff2],
 };
 
 const sfMonoNormalWeights = {
-  400: [SFMonoRegularWoff, SFMonoRegularWoff2],
-  600: [SFMonoSemiboldWoff, SFMonoSemiboldWoff2],
+    400: [SFMonoRegularWoff, SFMonoRegularWoff2],
+    600: [SFMonoSemiboldWoff, SFMonoSemiboldWoff2],
 };
 
 const sfMonoItalicWeights = {
-  400: [SFMonoRegularItalicWoff, SFMonoRegularItalicWoff2],
-  600: [SFMonoSemiboldItalicWoff, SFMonoSemiboldItalicWoff2],
+    400: [SFMonoRegularItalicWoff, SFMonoRegularItalicWoff2],
+    600: [SFMonoSemiboldItalicWoff, SFMonoSemiboldItalicWoff2],
 };
 
 const calibre = {
-  name: 'Calibre',
-  normal: calibreNormalWeights,
-  italic: calibreItalicWeights,
+    name: 'Calibre',
+    normal: calibreNormalWeights,
+    italic: calibreItalicWeights,
 };
 
 const sfMono = {
-  name: 'SF Mono',
-  normal: sfMonoNormalWeights,
-  italic: sfMonoItalicWeights,
+    name: 'SF Mono',
+    normal: sfMonoNormalWeights,
+    italic: sfMonoItalicWeights,
 };
 
 const createFontFaces = (family, style = 'normal') => {
-  let styles = '';
+    let styles = '';
 
-  for (const [weight, formats] of Object.entries(family[style])) {
-    const woff = formats[0];
-    const woff2 = formats[1];
+    for (const [weight, formats] of Object.entries(family[style])) {
+        const woff = formats[0];
+        const woff2 = formats[1];
 
-    styles += `
+        styles += `
       @font-face {
         font-family: '${family.name}';
         src: url(${woff2}) format('woff2'),
             url(${woff}) format('woff');
         font-weight: ${weight};
         font-style: ${style};
-        font-display: auto;
+        font-display: swap;
       }
     `;
-  }
+    }
 
-  return styles;
+    return styles;
 };
 
 const calibreNormal = createFontFaces(calibre);
